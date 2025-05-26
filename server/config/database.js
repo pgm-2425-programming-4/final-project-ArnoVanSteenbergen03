662f5432608c238bfacb264855ac18e5d1d17986
ArnoVanSteenbergen03 <arno.vansteenbergen@student.arteveldehs.be>
2025-05-26 13:18:11 +0200

    another try to deploy on render

--- server/config/database.js
+++ server/config/database.js
@@ -1,19 +1,39 @@
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
+const parse = require('pg-connection-string').parse;
+
+module.exports = ({ env }) => {
+  const isProduction = env('NODE_ENV') === 'production';
+
+  if (isProduction) {
+    const config = parse(env('DATABASE_URL'));
+
+    return {
+      connection: {
+        client: 'postgres',
+        connection: {
+          host: config.host,
+          port: config.port,
+          database: config.database,
+          user: config.user,
+          password: config.password,
+          ssl: {
+            rejectUnauthorized: false,
+          },
+        },
+      },
+    };
+  }
+
+  return {
     connection: {
-      host: env('DATABASE_HOST'),
-      port: env.int('DATABASE_PORT'),
-      database: env('DATABASE_NAME'),
-      user: env('DATABASE_USERNAME'),
-      password: env('DATABASE_PASSWORD'),
-      ssl: env.bool('DATABASE_SSL', false) ? {
-        rejectUnauthorized: false
-      } : false,
+      client: 'postgres',
+      connection: {
+        host: env('DATABASE_HOST', 'localhost'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'postgres'),
+        user: env('DATABASE_USERNAME', 'postgres'),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool('DATABASE_SSL', false),
+      },
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
-});
+  };
+};
