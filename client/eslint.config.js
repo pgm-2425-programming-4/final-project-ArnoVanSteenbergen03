0f6682b1df40eaace2a4f4351af4be8d50a5a606
ArnoVanSteenbergen03 <arno.vansteenbergen@student.arteveldehs.be>
2025-06-08 14:17:59 +0200

    added eslint + prettier tool

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended"
 
 export default [
   { ignores: ['dist'] },
@@ -19,6 +20,7 @@ export default [
     plugins: {
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'prettier': eslintPluginPrettierRecommended,
     },
     rules: {
       ...js.configs.recommended.rules,
